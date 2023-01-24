98321705a91723d58ffffa388655b92c270fbd82
Your Name <you@example.com>
2023-01-24 13:54:10 +0600

    adddall

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    client.connect();
+    await client.connect();
     const servicesCollection = client
       .db("watchServices")
       .collection("services");
